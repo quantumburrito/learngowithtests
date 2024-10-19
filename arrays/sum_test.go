9faaf1502c8f17507b5005e39d66c548a47a906c
quantumburrito <107977173+quantumburrito@users.noreply.github.com>
2024-10-19 14:53:30 -0500

    finished arrays and slices lesson

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -33,3 +33,25 @@ func TestSumAll(t *testing.T){
 
 	})
 }
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, want, got []int) {
+		t.Helper()
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Expected %v, Got %v", want, got)
+		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1,2}, []int{0,9})
+		want := []int{2,9}
+
+		checkSums(t,want, got)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3,4,5})
+		want := []int{0,9}
+		checkSums(t,want,got)
+	})
+	
+}
