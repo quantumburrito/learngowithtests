a550bca99ae5445dce41d3a75b66cc448b889af8
quantumburrito <107977173+quantumburrito@users.noreply.github.com>
2024-10-19 07:02:16 -0500

    finished first lesson

--- hello/hello.go
+++ hello/hello.go
@@ -3,14 +3,47 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const englishWorld = "World"
+const frenchWorld = "Monde"
+const spanishWorld = "Mundo"
+const spanish = "Spanish"
+const english = "English"
+const french = "French"
 
-func Hello(name string) string {
-	if(name == "") {
-		name = "World"
+func Hello(name, language string) string {
+	return greetingPrefix(language) + worldWord(name, language) + "!"
+}
+
+func worldWord(name string, language string) (worldname string) {
+	if name == "" {
+		switch language {
+		case spanish:
+			worldname = spanishWorld
+		case french:
+			worldname = frenchWorld
+		default:
+			worldname = englishWorld
+		}
+	} else {
+		worldname = name
 	}
-	return englishHelloPrefix + name + "!"
+	return
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
