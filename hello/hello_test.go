98a2a6bbd927861d0ca30052acbc6b8560f65d27
quantumburrito <107977173+quantumburrito@users.noreply.github.com>
2024-10-19 06:32:22 -0500

    feature: empty string yields Hello, World!

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,19 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Bryce")
-	want := "Hello, Bryce!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Bryce")
+		want := "Hello, Bryce!"
 
-	if (got != want) {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if (got != want) {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, World!' when an empty string is supplied and no name is given", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+		if(got != want) {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
