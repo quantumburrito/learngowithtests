1843ae004c010b50a12c82b09cb7e9c697f05838
quantumburrito <107977173+quantumburrito@users.noreply.github.com>
2024-10-26 20:46:15 -0500

    implemented bitcoin

--- banking/wallet.go
+++ banking/wallet.go
@@ -4,15 +4,22 @@ import "fmt"
 
 type Bitcoin int
 
-type Wallet struct {
-	balance int
+type Stringer interface{
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
-	fmt.Printf("Address of the balance in wallet is:%p \n", &w.balance)
+func (w *Wallet) Balance() Bitcoin {
 	return (*w).balance
 }
--- banking/wallet_test.go
+++ banking/wallet_test.go
@@ -8,14 +8,14 @@ import (
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
-	
+
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
-		t.Errorf("Got: %d Want: %d", got, want)
+		t.Errorf("Got: %s Want: %s", got, want)
 	}
 }
